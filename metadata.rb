594a85d9a1e086dedfe92b6294c2af2c670e2d8d
Paul Walger <metaxy@walger.name>
2013-02-18 12:49:38 +0100

    better file#names

--- metadata.rb
+++ metadata.rb
@@ -1,36 +1,33 @@
 require 'taglib'
 
 def writeid3(file)
-   frame_factory = TagLib::ID3v2::FrameFactory.instance
-   frame_factory.default_text_encoding = TagLib::String::UTF8
-    TagLib::MPEG::File.open(file) do |file|
+    frame_factory = TagLib::ID3v2::FrameFactory.instance
+    frame_factory.default_text_encoding = TagLib::String::UTF8
+    TagLib::MPEG::File.open file do |file|
         tag = file.id3v2_tag
         tag.album = $options[:serie]
         tag.year = Date.parse($options[:date]).year
         tag.comment = "Aufnahme der ECG Berlin http://ecg-berlin.de"
         tag.artist = $options[:preacher]
-     #   tag.date = $options[:date]
-     #   tag.language = $options[:lang]
         tag.title = $options[:title]
         file.save
     end
-
-end 
+end
+def clean(old)
+    old.sub(" ", "-").sub(",", "-").sub("(", "").sub(")", "")
+end
 def rename(old)
     newName = old
     cat = $catNames[$options[:cat]]
     ref = ""
     ref = " " + ref + " " if $options[:ref] != "" and $options[:ref] != nil
-    if(File.extname(old) == ".mp3")
-        newName = File.dirname(old) + "/#{$options[:date]} #{cat} - #{ref}#{$options[:title]} (#{$options[:preacher]}).mp3"
-        File.rename(old, newName)
-    elsif(File.extname(old) == ".mp4")
-        newName = File.dirname(old) + "/#{$options[:date]} #{cat} - #{ref}#{$options[:title]} (#{$options[:preacher]}).mp4"
-        File.rename(old, newName)
+    if(File.extname(old) == ".mp3" || File.extname(old) == ".mp4")
+        newName = File.dirname(old) + 
+                "/#{$options[:date]} #{cat} - #{ref}#{$options[:title]} (#{$options[:preacher]})" + 
+                File.extname(old)
     else
-     # newName = old
-      #newName.sub!(" ","-")
-      #newName.sub!(",","-")
+        newName = File.dirname(old) + clean File.basename(old) + File.extname(old)
     end
+    File.rename(old, newName)
     newName
 end
