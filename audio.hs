0432804825aa5af7ca3c6bb4dd445d52848412a5
Paul Walger <metaxy@walger.name>
2013-05-25 17:23:09 +0200

    hs

--- audio.hs
+++ audio.hs
@@ -9,23 +9,31 @@ import           Data.Iteratee.STM
 import           Control.Monad.CatchIO
 import           System.Environment
 import  Data.Iteratee.Iteratee
-import Control.Monad.IO.Class
+import  Data.Iteratee.ListLike
+import Control.Monad.IO.Class as M
 
-main :: IO ()
+main :: IO ([Double])
 main = do
     let e2 = enumAudioIteratee "out.wav"
-    e <- e2 maxIter >>= run
-    e1 <- e2 maxIter2 >>= run
+    e <- e2 (maxIter >> maxIter) >>= run
     print e
+    return e
+alla :: IO([Double])
+alla = do
+    let e2 = enumAudioIteratee "out.wav"
+    e <- e2 maxIter2 >>= run
+    return e
 
---maxIter :: MonadCatchIO m => Iteratee (V.Vector Double) m [Double]
---maxIter = [
---
-maxIter :: MonadCatchIO m => Iteratee (V.Vector Double) m Double
-maxIter = I.length
 
---maxIter2 :: MonadCatchIO m => Iteratee (V.Vector Double) m ()
-maxIter2 = I.sum
+--maxIter :: MonadCatchIO m => Iteratee (V.Vector Double) m Int
+--maxIter = I.length
+
+maxIter :: MonadCatchIO m => Iteratee (V.Vector Double) m [Double]
+maxIter = joinI $ (I.take 50) I.stream2list
+
+maxIter2 :: MonadCatchIO m => Iteratee (V.Vector Double) m [Double]
+maxIter2 = joinI $ (I.take 100) I.stream2list
 
 byteCounter :: Monad m => Iteratee (V.Vector Double) m Int
 byteCounter = I.length
+
