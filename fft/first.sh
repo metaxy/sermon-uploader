a30a97a6e8e131b9b134a76f011974c3bbdf2796
Paul Walger <metaxy@walger.name>
2013-07-24 15:11:04 +0200

    g++-4.7

--- fft/first.sh
+++ fft/first.sh
@@ -2,13 +2,13 @@ git clone https://github.com/zsiciarz/aquila.git
 cd aquila
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_CXX_COMPILER=g++-4.7 ..
 make
 sudo make install
 cd ../..
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_CXX_COMPILER=g++-4.7 ..
 make
 
 
