3ed648b0a4e658d49123142332beca35c6024590
Paul Walger <metaxy@walger.name>
2013-07-25 09:07:29 +0200

    update

--- fft/main.cpp
+++ fft/main.cpp
@@ -118,7 +118,7 @@ int main(int argc, char *argv[])
     }
     double lc2 = small.getAudioLength();
     
-    cout << setiosflags(ios::fixed) << setprecision(0) << g_secs << ";" << (lc2*1000) << ";" << id;
+    cout << setiosflags(ios::fixed) << setprecision(0) << g_secs << ";" << (lc2/1000) << ";" << id;
     return 0;
 }
 
