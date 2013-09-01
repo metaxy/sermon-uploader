43003f4211eb566aca8a8c1ace4a538efe90dd90
Paul Walger <metaxy@walger.name>
2013-09-01 19:44:26 +0200

    update

--- create.sh
+++
@@ -1,2 +0,0 @@
-ffmpeg -i 1.wav -ar 5000 -ac 1 out.wav
-ffmpeg -i 2.wav -ar 5000 -ac 1 out2.wav
--- spect.sh
+++
@@ -1,2 +0,0 @@
-sox out.wav -n spectrogram -x 5000 -o "1.png"
-sox out2.wav -n spectrogram  -x 5000 -o "2.png"
