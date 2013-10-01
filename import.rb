a05ea48b98cf154622671cf0a5dbdd50383a868b
test <test@ecg-berlin.de>
2013-10-01 08:51:20 +0200

    import

---
+++ import.rb
@@ -0,0 +1,30 @@
+ #encoding: utf-8
+require 'csv'
+require 'ftools'
+
+$csvFile = "./jos_predigtenFile.csv";
+$folder = "/var/www/vhosts/ecg-berlin.de/httpdocs/fileadmin/files/hellersdorf/predigten/";
+$newpath = "/home/ecg-media/import/";
+$id = 0;
+$prediger = 2;
+$title = 3;
+$path = 4;
+$date = 5;
+
+
+
+def main()
+    CSV.foreach($csvFile) do |row|
+        next if row[$id].to_i < 750
+        puts "id : #{row[$id]}"
+        puts "prediger : #{row[$prediger]}"
+        puts "title : #{row[$title]}"
+        puts "path : #{row[$path]}"
+        puts "date : #{row[$date]}"
+        folder = "#{$newpath}#{row[$title]}/"
+        Dir.mkdir(folder)
+        File.copy("#{$folder}#{row[$path]}", "#{folder}#{row[$date]} =  = #{row[$prediger]}.mp3")
+    end
+end
+
+main();
