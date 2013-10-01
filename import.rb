95ba8cfa523a2e56a172c6fe5e9398fbcd6b03e4
Paul Walger <metaxy@walger.name>
2013-10-01 11:08:41 +0200

    update

--- import.rb
+++ import.rb
@@ -23,11 +23,12 @@ def main()
         puts "date : #{row[$date]}"
         row[$title] = "#{row[$date]} von #{row[$prediger]}" if row[$title] == "" or row[$title] == nil
         folder = "#{$newpath}#{row[$title]}/"
+        next if not File.exists?" #{$folder}#{row[$path]}"
         Dir.mkdir(folder)
         row[$prediger] = "Alexander Arzer" if row[$prediger] == "Alex Arzer"
         row[$prediger] = "Wilhelm Walger" if row[$prediger] == "Willi Walger"
         row[$prediger] = "Wadim Ruff" if row[$prediger] == "Wadim Ruf"
-        next if not File.exists?" #{$folder}#{row[$path]}"
+       
         File.copy("#{$folder}#{row[$path]}", "#{folder}#{row[$date]} =  = #{row[$prediger]}.mp3")
     end
 end
