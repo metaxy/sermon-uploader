6a478290ddfe87de94d03eb0a26695451e2210b4
Paul Walger <metaxy@walger.name>
2013-10-18 17:43:26 +0200

    update

--- cron.rb
+++ cron.rb
@@ -132,11 +132,11 @@ def main
     # scan
     $logger.debug "start"
     Dir.glob($options[:newHome] + "**/*").each do |item| # scan all folders
-        sleep 4
+       
         next if item == '.' or item == '..' # skip
         next if(not File.directory? item) # skip files
         cleanOptions() # new option
-
+        sleep 1
         next if addFile(item) != :ok # add all files in this dir
         next if error_check($options) == :failed
         
