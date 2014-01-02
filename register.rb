b05f42a66bdd0eb867f7e7140e8bdddc72ae0cb8
Paul Walger <metaxy@walger.name>
2014-01-02 10:47:24 +0100

    update

--- register.rb
+++ register.rb
@@ -7,7 +7,9 @@ def files_data(file_names)
     file_names.each do |file_name|
         #todo: get file type
         file_type = "other"
+        puts "before #{file_name}"
         file_name[$options[:visible_path]] = "/"
+        puts "after #{file_name}"
         suffix = File.extname(file_name).downcase
         #puts "#{suffix} of #{file_name}"
         if(suffix == ".mp3" or suffix == ".ogg")
