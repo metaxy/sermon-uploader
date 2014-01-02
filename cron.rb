e418130d2a397877355fd738a1aa6885e4df2cfb
Paul Walger <metaxy@walger.name>
2014-01-02 10:22:45 +0100

    update

--- cron.rb
+++ cron.rb
@@ -195,7 +195,8 @@ def main
         puts $options[:videoPath].has_key?($options[:group_name])
         puts $options[:autoVideo]
         puts has_videos
-        if($options[:videoPath].has_key?($options[:group_name]) && $options[:autoVideo] == true && has_videos == :no)
+        puts "options ::: !!!!!!!!!!::::!!!!!!!!"
+        if($options[:videoPath].has_key?(file_info[:group_name]) && $options[:autoVideo] == true && has_videos == :no)
             add_video(file_info)
         end
         
