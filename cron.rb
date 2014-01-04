e78efa0401f7c42d99b399bed1f7b848b54f161e
Paul Walger <metaxy@walger.name>
2014-01-04 16:34:49 +0100

    update

--- cron.rb
+++ cron.rb
@@ -22,7 +22,7 @@ end
 def add_file(path)
     file_info = Hash[]
     files = find_all_files(path)
-    files = files.select! { |f| File.extname(f).downcase != ".mp4" }
+    files.select! { |f| File.extname(f).downcase != ".mp4" }
     mp3s = files.select { |f| File.extname(f).downcase == ".mp3" }                  
     return nil if mp3s.empty?
     mp3 = mp3s.first
